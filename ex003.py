25454533583bf37d9c7991492f75c61ebcd0c586
danielsilvagoms <danielsilva.goms@gmail.com>
2026-08-27 11:59:59 -0300

    Update ex003.py

--- ex003.py
+++ ex003.py
@@ -1,3 +1,5 @@
+'''Exercício Python 003: Crie um programa que leia dois números e mostre a soma entre eles.'''
+
 n1 = int(input('Digite um valor: '))
 n2 = int(input('Digite outro valor: '))
 print(f'A soma entre {n1} e {n2} é igual a {n1+n2}')
