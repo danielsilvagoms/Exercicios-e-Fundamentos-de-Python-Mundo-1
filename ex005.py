a8cf81a7531a294ee1f1252604f62aefcb0144a8
danielsilvagoms <danielsilva.goms@gmail.com>
2026-09-07 20:54:24 -0300

    Update ex005.py

--- ex005.py
+++ ex005.py
@@ -1,3 +1,5 @@
+'''Exercício Python 005: Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.'''
+
 num = int(input('Digite um número:'))
 a = num - 1
 s = num + 1
