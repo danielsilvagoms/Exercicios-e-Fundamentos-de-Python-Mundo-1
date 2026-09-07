8f336cbc0e100ade929576ea56f888ba8da4f5ab
danielsilvagoms <danielsilva.goms@gmail.com>
2026-09-07 20:56:02 -0300

    Update ex010.py

--- ex010.py
+++ ex010.py
@@ -1,2 +1,4 @@
+'''Exercício Python 010: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.'''
+
 din = float(input('Quanto dinheiro você tem na carteira? R$'))
 print(f'Com R${din} você pode comprar US${din/5.08:.2f}')
