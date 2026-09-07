342a7b4dcec6c9caa3c6390fbce665b378a524fb
danielsilvagoms <danielsilva.goms@gmail.com>
2026-09-07 20:58:03 -0300

    Update ex018.py

--- ex018.py
+++ ex018.py
@@ -1,3 +1,6 @@
+'''Exercício Python 018: Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.'''
+
+
 from math import radians, sin, cos, tan
 angulo = float(input('Digite o angulo que você deseja: '))
 seno = sin(radians(angulo))
