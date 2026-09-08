09937c1b31bb737dc5fb9ca43a994516a5539bad
danielsilvagoms <danielsilva.goms@gmail.com>
2026-09-07 21:08:00 -0300

    Update ex026.py

--- ex026.py
+++ ex026.py
@@ -1,3 +1,6 @@
+'''Exercício Python 026: Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A", em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.'''
+
+
 frase = str(input('Digite uma frase: ')).strip().upper()
 print(f'A letra A aparece {frase.count('A')} vezes na frase')
 print(f'A primeira letra A aparece na posição {frase.find('A')+1}')
