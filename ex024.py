b63ee506e0fda43414544579a48e0253263cb9cf
danielsilvagoms <danielsilva.goms@gmail.com>
2026-09-07 21:01:33 -0300

    Update ex024.py

--- ex024.py
+++ ex024.py
@@ -1,3 +1,5 @@
+'''Exercício Python 021: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.'''
+
 cidade = input('Em que cidade você nasceu? ').upper().split()
 print('Começa com SANTO?')
 print(cidade[0] == 'SANTO')
